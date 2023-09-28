b547df71854433d7388d43f812dc87f7b8243073
Quentin Laffont <quentin@flexper.fr>
2023-09-28 07:10:07 +0200

    feat(app): add signle instance check

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,16 +4,16 @@
 )]
 
 use std::thread;
-use tauri::{Manager, Size, LogicalSize};
+use tauri::{LogicalSize, Manager, Size, UserAttentionType};
 mod commands;
-mod process_list;
+mod filepath;
 #[cfg_attr(target_os = "windows", path = "music.rs")]
 #[cfg_attr(not(target_os = "windows"), path = "music_other.rs")]
 mod music;
-mod filepath;
-use tauri::{CustomMenuItem, SystemTrayMenu, SystemTrayMenuItem, SystemTrayEvent, SystemTray};
-use tauri_plugin_autostart::MacosLauncher;
+mod process_list;
 use async_std::task;
+use tauri::{CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem};
+use tauri_plugin_autostart::MacosLauncher;
 
 fn main() {
     let mut is_autostart_enabled = false;
@@ -23,7 +23,14 @@ fn main() {
     });
 
     let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-    let hide = CustomMenuItem::new("hide".to_string(), if is_autostart_enabled == true {"Show"} else {"Hide"});
+    let hide = CustomMenuItem::new(
+        "hide".to_string(),
+        if is_autostart_enabled == true {
+            "Show"
+        } else {
+            "Hide"
+        },
+    );
     let mut auto_start = CustomMenuItem::new("auto_start".to_string(), "Start on boot");
 
     if is_autostart_enabled == true {
@@ -31,15 +38,23 @@ fn main() {
     }
 
     let tray_menu = SystemTrayMenu::new()
-    .add_item(hide)
-    .add_item(auto_start)
-    .add_native_item(SystemTrayMenuItem::Separator)
-    .add_item(quit);
+        .add_item(hide)
+        .add_item(auto_start)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(quit);
 
     let system_tray = SystemTray::new().with_menu(tray_menu);
 
     tauri::Builder::default()
-        .plugin(tauri_plugin_autostart::init(MacosLauncher::LaunchAgent, Some(vec![])))
+        .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
+            let window = app.get_window("main").unwrap();
+            window.set_focus();
+            window.request_user_attention(Some(UserAttentionType::Informational));
+        }))
+        .plugin(tauri_plugin_autostart::init(
+            MacosLauncher::LaunchAgent,
+            Some(vec![]),
+        ))
         .system_tray(system_tray)
         .setup(move |app: &mut tauri::App| {
             let window = app.get_window("main").unwrap();
@@ -48,8 +63,14 @@ fn main() {
 
             window.set_closable(false);
             window.set_maximizable(false);
-            window.set_min_size(Some(Size::Logical(LogicalSize { width: 400.0, height: 600.0 })));
-            window.set_max_size(Some(Size::Logical(LogicalSize { width: 675.0, height: 600.0 })));
+            window.set_min_size(Some(Size::Logical(LogicalSize {
+                width: 400.0,
+                height: 600.0,
+            })));
+            window.set_max_size(Some(Size::Logical(LogicalSize {
+                width: 675.0,
+                height: 600.0,
+            })));
 
             if is_autostart_enabled {
                 window.hide();
@@ -82,31 +103,30 @@ fn main() {
         ])
         .on_system_tray_event(|app, event| match event {
             SystemTrayEvent::MenuItemClick { id, .. } => {
-            let item_handle = app.tray_handle().get_item(&id);
-              match id.as_str() {
-                "quit" => {
-                  std::process::exit(0);
-                }
-                "hide" => {
-                    let window = app.get_window("main").unwrap();
-                    if !(window.is_visible().unwrap()) {
-                        window.show().unwrap();
-                        item_handle.set_title("Hide").unwrap();
-                    }else {
-                        window.hide().unwrap();
-                        item_handle.set_title("Show").unwrap();
+                let item_handle = app.tray_handle().get_item(&id);
+                match id.as_str() {
+                    "quit" => {
+                        std::process::exit(0);
                     }
+                    "hide" => {
+                        let window = app.get_window("main").unwrap();
+                        if !(window.is_visible().unwrap()) {
+                            window.show().unwrap();
+                            item_handle.set_title("Hide").unwrap();
+                        } else {
+                            window.hide().unwrap();
+                            item_handle.set_title("Show").unwrap();
+                        }
+                    }
+                    "auto_start" => {
+                        let window = app.get_window("main").unwrap();
+                        window.emit("toggle_autostart", "").unwrap();
+                    }
+                    _ => {}
                 }
-                "auto_start" => {
-                    let window = app.get_window("main").unwrap();
-                    window.emit("toggle_autostart", "").unwrap();
-                }
-                _ => {}
-              }
             }
             _ => {}
-          })
+        })
         .run(tauri::generate_context!())
-
         .expect("error while running tauri application");
 }
